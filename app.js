6e257ab0d23aa8ad5ffe075feee7cbb7fdc84ed2
1994reka3 <rik.s.1005@gmail.com>
2021-01-13 23:52:08 +0900

    [update]じゃんけんプログラム追加

--- app.js
+++ app.js
@@ -29,3 +29,63 @@ for(i = 1; i < 11; i ++){
   num = num + i;
 }
 alert('1から10まで足し算した結果は' + num + 'です');
+
+//関数
+var alertString;
+alertString = addString("WebCamp");
+alert(alertString);
+function addString(strA){
+  var addStr = "Hello" + strA;
+  return addStr;
+}
+
+var promptStr = prompt("好きな文字を入力してください");
+alert(promptStr);
+
+//じゃんけん
+var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選択');
+var js_hand = getJshand();
+var judge = winLose(user_hand, js_hand);
+alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+
+function getJshand(){
+  var js_hand_num = Math.floor( Math.random() * 3 );
+  var hand_name;
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  }else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  }else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+  return hand_name;
+}
+function winLose(user, js){
+  var winLoseStr;
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    }else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    }else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  }else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    }else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    }else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  }else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    }else if(js == "チョキ"){
+      winLoseStr = "負け";
+    }else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+  return winLoseStr;
+}
