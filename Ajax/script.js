c9adb1885f26efa5fc54860a022a3ef2918f3f9a
1994reka3 <rik.s.1005@gmail.com>
2021-02-11 00:47:27 +0900

    [update]Ajax練習

--- Ajax/script.js
+++ Ajax/script.js
@@ -6,7 +6,16 @@ $(function(){
       url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + "9ceda7597ff2347635da4cc58bcb07c1",
       dataType: 'jsonp',
     }).done(function (data){
+      $('#place').text(data.name);
+      $('#temp_max').text(data.main.temp_max);
+      $('#temp_min').text(data.main.temp_min);
+      $('#humidity').text(data.main.humidity);
+      $('#speed').text(data.wind.speed);
+      $('#weather').text(data.weather[0].main);
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
     }).fail(function (data){
+      alert('通信に失敗しました');
     });
   });
 });
